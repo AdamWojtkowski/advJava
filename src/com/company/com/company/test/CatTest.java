ca03e71400472bfdb2341aba6f9422ef9cd9254f
Adam Wojtkowski <awojtkowski@my.wctc.edu>
2018-01-29 11:43:47 -0600

    what

--- src/com/company/com/company/test/CatTest.java
+++ src/com/company/com/company/test/CatTest.java
@@ -1,8 +1,12 @@
 package com.company.com.company.test;
 
-
 import com.company.Cat;
 import org.junit.Test;
+
+/**
+ * @author Adam Wojtkowksi
+ * @version 1.0
+ */
 
 public class CatTest extends junit.framework.TestCase  {
 
@@ -10,17 +14,39 @@ public class CatTest extends junit.framework.TestCase  {
     @Test
     public void testSomething() {
 
-       Cat[] cats = {new Cat( 6, "John"),
-                     new Cat( -1,"Adam")};
-        int i=0;
-        for ( Cat item : cats) {
-            assertFalse("A cat can't kill negative mice! " + item.getMousesKilled(), item.getMousesKilled() < 0);
+/**
+ * @param
+ * Created Cat objects to test mice killed,
+ * was not below zero.
+ */
+        Cat[] cats = {new Cat( 6, "John"),
+                new Cat( -1,"Adam")};
 
+/**
+ * @param
+ * An int variable to iterate over the cats array
+ */
+        int i=0;
+        /**
+         * For loop to run and check if Cat object
+         * getMousesKilled() method returns a number below zero
+         */
+        for ( Cat item : cats) {
+
+            assertFalse("A cat can't kill negative mice! " + item.getMousesKilled(), item.getMousesKilled() < 0);
+            /**
+             * @exception
+             * Output mice killed or assertFalse boolean message
+             */
             System.out.println("This is mice killed: " + cats[i] + " " + item.talk());
             i++;
         }
     }
-   // @Override
+
+    /**
+     * @return
+     * Returns a message when the anaimal talks.
+     */
     public String talk() {
         return "Meow!";
     }
--- src/com/company/com/company/test/DogTest.java
+++ src/com/company/com/company/test/DogTest.java
@@ -1,4 +1,48 @@
 package com.company.com.company.test;
+import com.company.Dog;
+import org.junit.Test;
+/**
+ * @author Adam Wojtkowksi
+ * @version 1.0
+ */
+public class DogTest extends junit.framework.TestCase {
+    @Test
+    public void testSomething() {
 
-public class DogTest {
+/**
+ * @param
+ * Created Dog objects to test if friendly is false
+ */
+        Dog[] dogs = {new Dog( true, "John"),
+                new Dog( true,"Adam")};
+
+    /**
+     * @param
+     * An int variable to iterate over the dogs array
+     */
+    int i=0;
+    /**
+     * For loop to run and check if Dog object
+     * isFriendly() method returns a number below zero
+     */
+        for ( Dog item : dogs) {
+
+       // assertFalse("This Dog is not friendly! " + item.isFriendly(), item.isFriendly() == false);
+        /**
+         * @exception
+         * Output is friendly or assertTrue boolean message
+         */
+        assertTrue("This Dog is friendly! " + item.isFriendly(), item.isFriendly() != true);
+        System.out.println("This is the dog: " + dogs[i] + " " + item.talk());
+        i++;
+    }
+}
+    /**
+     * @return
+     * Returns a message when the anaimal talks.
+     */
+    public String talk() {
+        return "Woof!";
+    }
+
 }
